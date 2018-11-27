53ac41fa0ad84fcefee6ea9bace8894f81cf996f
Miles Exner <mexner@pacificcollege.edu>
2018-11-27 15:36:57 -0800

    Add JSON Serialization

---
+++ Syllabus Generator/Djson.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syllabus_Generator
+{
+    class Djson
+    {
+        private object _data;
+        private string _json;
+        private List<Assignment> _assignments;
+        private List<Term> _terms;
+
+        public Djson (object _data)
+        {
+            this._data = _data;
+
+            _json = JsonConvert.SerializeObject(_data);
+            saveJsonFile(_json);
+        }
+
+        private object readJsonFile(string fileName, object Data)
+        {
+
+            string output = "";
+
+            return JsonConvert.DeserializeObject<Assignment>(output);
+        }
+
+        private void saveJsonFile(string json)
+        {
+
+        }
+
+    }
+}
---
+++ Syllabus Generator/Term.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syllabus_Generator
+{
+    class Term
+    {
+        private int _id;
+        private string _title;
+        private string _description;
+        private DateTime _dateRange;
+        private List<string> _weeks;
+    }
+}
