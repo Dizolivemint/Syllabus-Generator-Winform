34a60e0f85b52d654cfc9bff31d5bdaa9814b222
Miles Exner <mexner@pacificcollege.edu>
2018-12-04 11:16:29 -0800

    Add json file writing

--- Syllabus Generator/Djson.cs
+++ Syllabus Generator/Djson.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,29 +12,103 @@ namespace Syllabus_Generator
     {
         private object _data;
         private string _json;
-        private List<Assignment> _assignments;
-        private List<Term> _terms;
+        private string _dir;
+        private string _path;
+
+        private FileStream fs;
+        private StreamWriter textOut;
+        private StreamReader textIn;
 
         public Djson (object _data)
         {
             this._data = _data;
+            
+            _dir = AppDomain.CurrentDomain.BaseDirectory;
 
+            _path = $"{_dir}\\db.json";
             _json = JsonConvert.SerializeObject(_data);
-            saveJsonFile(_json);
+
+            CreateFile();
+
+            SaveFile(_json);
         }
 
-        private object readJsonFile(string fileName, object Data)
+
+        private object ReadJsonFile(string fileName, object Data)
         {
 
             string output = "";
 
-            return JsonConvert.DeserializeObject<Assignment>(output);
+            return JsonConvert.DeserializeObject<object>(output);
         }
 
-        private void saveJsonFile(string json)
+        public string path
         {
-
+            get { return _path; }
         }
 
+        public bool FileDoesExist()
+        {
+            if (!File.Exists(this._path))
+            {
+                return false;
+            }
+            else if (new FileInfo(this._path).Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public string CreateFile()
+        {
+            fs = null;
+
+            if (!Directory.Exists(this._dir))
+            {
+                try
+                {
+                    Directory.CreateDirectory(this._dir);
+                }
+                catch
+                {
+                    return "Cannot create directory";
+                }
+            }
+
+            try
+            {
+                fs = new FileStream(this._path, FileMode.OpenOrCreate, FileAccess.Write);
+            }
+            catch (IOException ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+
+            fs.Close();
+            return "Success";
+        }
+
+        public bool SaveFile(string data)
+        {
+            textOut =
+                new StreamWriter(
+                    new FileStream(this._path, FileMode.Append, FileAccess.Write));
+
+            textOut.WriteLine(data);
+
+            textOut.Close();
+            return true;
+        }
     }
 }
